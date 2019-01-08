fb9b344d82c2b880ed12428c2c7e6aef39575de1
Martijn van Laar <martijn@mvanlaar.net>
2019-01-08 14:24:46 +0100

    Add project files.

---
+++ MvL-DuoGeoLoc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvL_DuoGeoLoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
